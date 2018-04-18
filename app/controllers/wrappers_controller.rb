053f48ea9e8a1e65abe1a109af37d2a985639ebe
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-18 18:18:57 -0500

    remove persisted for instance of Wrapper

--- app/controllers/wrappers_controller.rb
+++ app/controllers/wrappers_controller.rb
@@ -2,7 +2,7 @@ class WrappersController < JSONAPI::ResourceController
   def create
   	if params[:page].present?
   	  @wrapper = GrabbingService.new(params).perform
-  	  render json: @wrapper, status: 200 if @wrapper.persisted?
+  	  render json: @wrapper, status: 200
   	else
   	  render plain: 'Please provide at least one page'
   	end
