8809c14f467c6e9a73c46b7303da345f62206609
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-18 17:55:54 -0500

    change expect http code to expect Content-Type response

--- spec/requests/links_controller_spec.rb
+++ spec/requests/links_controller_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe LinksController, type: :controller do
   it "shows the previous links used before" do
     get :index
-    expect(response).to have_http_status(200)
+    expect(response.content_type).to eq "application/json"
   end
 end
