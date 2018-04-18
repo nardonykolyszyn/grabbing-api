3681034237c63e577ddf0cb59981bd85af5c82e3
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-18 18:54:35 -0500

    add shoulda matchers and create spec to wrapper model

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -29,7 +29,12 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
