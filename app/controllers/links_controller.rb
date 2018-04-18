fc80cb9c33015a6674374e9e05515b2af78a4976
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-18 17:53:29 -0500

    links index spec

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,10 @@
 class LinksController < ApplicationController
   def index
-  	@links = Wrapper.pluck(:id, :url)
-  	render json: @links
+    @links = Wrapper.pluck(:id, :url)
+    if @links.any?
+      render json: @links, status: 200
+    else
+      render plain: 'There are not any link provided yet'
+    end
   end
 end
