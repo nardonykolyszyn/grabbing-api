45302f80bf8e3c56c99b6c4d60814bb186d82212
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2018-04-18 19:00:53 -0500

    remove 4 spaces

--- app/controllers/wrappers_controller.rb
+++ app/controllers/wrappers_controller.rb
@@ -1,10 +1,10 @@
 class WrappersController < JSONAPI::ResourceController
   def create
-  	if params[:page].present?
-  	  @wrapper = GrabbingService.new(params).perform
-  	  render json: @wrapper, status: 200
-  	else
-  	  render plain: 'Please provide at least one page'
-  	end
+    if params[:page].present?
+      @wrapper = GrabbingService.new(params).perform
+      render json: @wrapper, status: 200
+    else
+      render plain: 'Please provide at least one page'
+    end
   end
 end
