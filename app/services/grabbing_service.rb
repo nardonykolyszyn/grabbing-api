0b0b80beae4dd65836a0525bdc3c5993bbae2136
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-08 08:09:31 -0500

    move payload builder to private method

--- app/services/grabbing_service.rb
+++ app/services/grabbing_service.rb
@@ -3,16 +3,15 @@
 require 'singleton'
 
 class GrabbingService
-
   include Singleton
 
-  TAGS = %w[h1 h2 h3 h4 p span b li].freeze
+  TAGS = %w[h1 h2 h3 h4 h5 p span b li strong].freeze
 
   def get_tags
     @page = HTTParty.get(@page_url)
     @dom = Nokogiri::HTML(@page)
     # Find tags and push them into the Payload.
-    @payload = Hash.new{ |hsh,key| hsh[key] = [] }
+    build_payload
     TAGS.each do |tag|
       @dom.xpath("//#{tag}").map { |obj| @payload[tag].push(obj.text) }
     end
@@ -23,12 +22,18 @@ class GrabbingService
     @wrapper = Wrapper.new(url: @page_url, tags: @payload)
     # If object is saved and persisted then return the object itself.
     # Else return the object errors as json format.
-    @wrapper.save ? @wrapper : @wrapper.errors.to_json 
+    @wrapper.save ? @wrapper : @wrapper.errors.to_json
   end
-  
+
   def perform(page_url)
     @page_url = page_url
     get_tags
     create_wrapper
+  end
+
+  private
+
+  def build_payload
+    @payload ||= Hash.new { |h, k| h[k] = [] }
   end
 end
